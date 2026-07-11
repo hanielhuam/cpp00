95043c4239514fe3d08bb846730b85dc02fcd9ef
Haniel Huam <hanielhuam@hotmail.com>
2026-07-11 14:48:46 -0300

    terminando o ex00

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -6,8 +6,25 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 18:58:36 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/07/09 18:58:53 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/07/11 14:45:28 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+int	main(int argc, char **argv)
+{
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+			std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
