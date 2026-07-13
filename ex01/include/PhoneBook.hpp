e51536d2dcb46ce37736785cedea4f54d5c5fa27
Haniel Huam <hanielhuam@hotmail.com>
2026-07-12 23:55:49 -0300

    começando o PhoneBook

--- ex01/include/PhoneBook.hpp
+++ ex01/include/PhoneBook.hpp
@@ -6,8 +6,24 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 14:59:46 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/07/11 14:59:48 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/07/12 23:54:03 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include "Contact.h"
+#define BOOK_SIZE 8
+
+class PhoneBokk
+{
+	private:
+		int		_size;
+		Contact _book[BOOK_SIZE];
+
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+		void	createContact(void);
+		void	search(void);
+};
--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -6,8 +6,36 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 14:56:11 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/07/11 14:56:19 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/07/12 23:53:58 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+#include <iostream>
 
+PhoneBook::PhoneBook(void)
+{
+	this->_size = 0;
+}
+
+PhoneBook::PhoneBook(void){}
+
+static Contact::Contact	buildContac(void)
+{
+	Contact		contact;
+	std::string firstName;
+	std::string lastName;
+	std::string nickName;
+	std::string phoneNumber;
+	std::string secreet;
+
+	std::cout << "insert first name" << std::endl;
+	std::cout << "insert last name" << std::endl;
+	std::cout << "insert nick name" << std::endl;
+	std::cout << "insert phone number" << std::endl;
+	std::cout << "insert secreet" << std::endl;
+}
+
+void	createContact(void)
+{
+}
