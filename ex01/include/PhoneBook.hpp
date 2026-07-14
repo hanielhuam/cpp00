860b1bd29b72c1a41bdd52de70aa470638dea6b8
Haniel Huam <hanielhuam@hotmail.com>
2026-07-13 23:58:14 -0300

    só ta faltando o main

--- ex01/include/PhoneBook.hpp
+++ ex01/include/PhoneBook.hpp
@@ -6,20 +6,23 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 14:59:46 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/07/12 23:54:03 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/07/13 23:57:09 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Contact.h"
-#define BOOK_SIZE 8
+#include <string>
+
+# define BOOK_SIZE 8
 
 class PhoneBokk
 {
 	private:
 		int		_size;
 		Contact _book[BOOK_SIZE];
+		void	showAllContacts(void);
 
 	public:
 		PhoneBook(void);
--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: hmacedo- <hanielhuam@hotmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/11 14:56:11 by hmacedo-          #+#    #+#             */
-/*   Updated: 2026/07/12 23:53:58 by hmacedo-         ###   ########.fr       */
+/*   Updated: 2026/07/13 23:57:03 by hmacedo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,21 @@ PhoneBook::PhoneBook(void)
 	this->_size = 0;
 }
 
-PhoneBook::PhoneBook(void){}
+std::string	getFieldInput(string err)
+{
+	std::string	input;
+
+	std::getLine(std::cin, input);
+	while (input.empty())
+	{
+		std::cout << "This Phone Book don't acept enpty input. " << err << endl;
+		std::getLine(std::cin, input);
+	}
+	return (input);
+}
 
 static Contact::Contact	buildContac(void)
 {
-	Contact		contact;
 	std::string firstName;
 	std::string lastName;
 	std::string nickName;
@@ -30,12 +40,74 @@ static Contact::Contact	buildContac(void)
 	std::string secreet;
 
 	std::cout << "insert first name" << std::endl;
+	fisrtName = getFieldInput("Enter the contact first name again");
 	std::cout << "insert last name" << std::endl;
+	lastName = getFieldInput("Enter the contact last name again");
 	std::cout << "insert nick name" << std::endl;
+	nickName = getFieldInput("Enter the contact nick name again");
 	std::cout << "insert phone number" << std::endl;
+	phoneNumber = getFieldInput("Enter the contact phone number again");
 	std::cout << "insert secreet" << std::endl;
+	secreet = getFieldInput("Enter the contact secreet again");
+	return (Contact(firstName, lastName, nickName, phoneNumber, secreet));
 }
 
 void	createContact(void)
 {
+	Contact	contact;
+	contact = buildContact();
+	this->book[this->size % BOOK_SIZE] = contact;
+}
+
+void	printLine(std::string data)
+{
+	if (data.size() > 10)
+		data = data.substr(0,9).append(".");
+	cout << "|" << std::setw(10) << data;
+}
+
+void	showAllContacts(void)
+{
+	std::cout << "|----------|----------|----------|----------|" << std::endl;
+	std::cout << "|    ID    |FIRST NAME|LAST NAME |NICK NAME |" << std::endl;
+	std::cout << "|----------|----------|----------|----------|" << std::endl;
+	for (int i = 0; i < this->_size && i < BOOK_SIZE; i++)
+	{
+		printLine(std::to_string(i + 1));
+		printLine(std::this->book[i].getFirstName());
+		printLine(std::this->book[i].getlastName());
+		printLine(std::this->book[i].getNickName());
+		std::cout << "|" << std::endl;
+	}
+	if (this
+	std::cout << "|__________|__________|__________|__________|" << std::emdl;
+}
+
+int	getInputId(int size)
+{
+	int			id;
+	std::string	input;
+	
+	std::cout << std::endl << "Enter a ID number:" << std::endl;
+	std::getLine(std::cin, input);
+	id = std::stoi(input);
+	while (id > 0 && id < size && id < BOOK_SIZE)
+	{
+		std::cout << "invalid ID. Please enter a valid ID:" << std::endl;
+		std::getLine(std::cin, input);
+		id = std::stoi(input);
+	}
+	return (id);
+}
+
+
+void	serch(void)
+{
+	int			id;
+
+	showAllContacts();
+	if (this->size == 0)
+		return ;
+	id = getInputId(this._size);
+	this->book[id - 1].showContact();
 }
